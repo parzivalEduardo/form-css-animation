2a93e0ba2a1e1b9d20aaad775a9ed7fb5b744a60
parzivalEduardo <dev_eduardo@hotmail.com>
2020-02-09 12:58:38 -0300

    add fields validation.

--- script.js
+++ script.js
@@ -5,7 +5,26 @@ const $body = document.querySelector('body');
 $btnEntrar.addEventListener('click', e => {
     e.preventDefault();
 
-    $form.classList.add('form-hide');
+    const fields = [...document.querySelectorAll('.input-block input')]
+
+    fields.forEach( field => {
+
+        if(field.value === '') $form.classList.add('validate-error');
+    })
+
+    const formError = document.querySelector('.validate-error')
+
+    if(formError) {
+        formError.addEventListener('animationend', e => {
+
+            if(e.animationName === 'nono'){
+                formError.classList.remove('validate-error')
+            }
+        })
+    }
+    else{
+        $form.classList.add('form-hide');
+    }
 
 });
 
